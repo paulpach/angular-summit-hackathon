fd3741b9aa7d2759861b1abec7f59829b229c468
Raju Gandhi <raju.gandhi+github@gmail.com>
2015-09-29 15:47:40 -0400

    feat: use strict DI for app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,19 +17,19 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  .config(["$routeProvider", function($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/main.html',
+          controller: 'MainCtrl',
+          controllerAs: 'main'
+        })
+        .when('/about', {
+          templateUrl: 'views/about.html',
+          controller: 'AboutCtrl',
+          controllerAs: 'about'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    }]);
